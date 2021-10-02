0d3c7f217a666610b69d078f7e34d211aa673ec2
Michael Su <michaelsu.jobs@gmail.com>
2021-10-02 14:40:32 -0700

    Implement deploy.js to mint NFT on-chain(testnet)

---
+++ blockchain/scripts/deploy.js
@@ -0,0 +1,23 @@
+const main = async () => {
+  const shinChanNFTContractFactory = await hre.ethers.getContractFactory('ShinChanNFT');
+  const shinChanNFTContract = await shinChanNFTContractFactory.deploy();
+  await shinChanNFTContract.deployed();
+  console.log("Contract deployed to:", shinChanNFTContract.address);
+  
+     // Calling the function to mint NFT
+  let txn = await shinChanNFTContract.makeShinChanNFT();
+  await txn.wait(); // waiting to be mined
+  console.log("Minted NFT #1");
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
